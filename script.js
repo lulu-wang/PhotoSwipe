e03af6645ec434058b02930cdeccc1276bcfa731
Nathan Pannell <nathan.pannell@berkeley.edu>
2018-04-01 05:58:46 -0700

    photos

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+var scroller;
+var scrollPosition;
+
 $(document).ready(function() {
-  var horizontalScroll;
-  console.log("whatup?");
+  scroller = $('.scroller');
+  var items = ['http://lorempixel.com/1000/1000/', 'http://lorempixel.com/1000/1000/'];
+  for (var src of items) {
+    scroller.append($('<img>', {
+      src: src
+    }));
+  }
   $(document).on('wheel', function(event) {
-    console.log("whatup wheel?");
+    updateSlider(event.originalEvent.deltaX);
   });
 });
+
+
+
+function updateSlider(delta) {
+  scrollPosition += delta;
+  scroller.css('transform', 'translateX(-' + scrollPosition + 'px)');
+}
