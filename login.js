8c0c8b16ddbf38cfe3365a0d9fe878764b04c8ef
Nathan Pannell <nathan.pannell@berkeley.edu>
2018-04-01 06:57:33 -0700

    ok

--- login.js
+++ login.js
@@ -1,3 +1,15 @@
 $(document).ready(funtion() {
-  
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var isAnonymous = user.isAnonymous;
+      uid = user.uid;
+
+      var personName = $('.enterNameField').val();
+      db.ref('users/' + uid).set({
+        name: personName,
+        collection: ["cool-story-bro"]
+      })
+    }
+  });
 });
--- script.js
+++ script.js
@@ -12,24 +12,7 @@ $(document).ready(function() {
     }));
   }
 
-  var db = firebase.database();
-  var uid;
-
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      var isAnonymous = user.isAnonymous;
-      uid = user.uid;
-
-      var personName = $('.enterNameField').val();
-      db.ref('users/' + uid).set({
-        name: personName,
-        collection: ["cool-story-bro"]
-      })
-    }
-  });
-
-  $('.enterBtn').click(function (){
+  $('.loginBtn').click(function (){
     firebase.auth().signInAnonymously().catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
