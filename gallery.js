72c53ec899107a1bf470c6baf0937691f9c81b35
Lulu Wang <lulu.wang25@gmail.com>
2018-04-01 11:39:50 -0700

    added changes

--- gallery.js
+++ gallery.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
   for (var i = 0; i < 20; i++) {
     console.log(i);
+    var result = 240 + i;
     $('.displayPhotos').append($('<div>', {
       class: 'poloroid',
     }).append($('<div>', {
       class: 'image'
-    }).css('background-image', 'url(https://loremflickr.com/320/' + 240 + i + ')')));
+    }).css('background-image', 'url(https://loremflickr.com/320/' + result + ')')));
   }
 });
